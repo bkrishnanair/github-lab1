19e3b012a9b84be133a5e49eb058f4580274bea1
balakrishnanair <balakrishna2352k@gmail.com>
2024-09-17 12:29:23 -0400

    search function implement ENPM611

--- search.py
+++ search.py
@@ -5,13 +5,12 @@ def search_json(json_data, search_string):
     # Place your search code here
     # you will have to loop through the json_data file you created earlier
     # finally you can store the match in the result list and return it
-    search_string = search_string.lower()  # Convert search string to lowercase for case-insensitive search
-    
-    # Loop through the JSON data
+    search_string = search_string.lower()
     for item in json_data:
-       
-        # Convert item values to string and check if the search string is in any of the values
         if any(search_string in str(value).lower() for value in item.values()):
             results.append(item)
-          
     return results
+
+#Step 1- converting searched words to lower case for case-insensitive results
+#Step 2- Looping through JSON Data items
+#Step 3- Converting Items to strings inorder to comapre with the search word, if yes retrun with results.
